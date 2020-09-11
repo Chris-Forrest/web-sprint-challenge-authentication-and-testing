9aeb88d0be0ad8d1c295504a137da1476ec48307
Chris Forrest <redrider_cr@yahoo.com>
2020-09-11 19:48:59 -0400

    made changes to package.json made auth-middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,21 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require("jsonwebtoken");
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+module.exports = async (req, res, next) => {
+  
+   try{
+        const token = req.headers.authorization;
+        if(!token){
+          return res.status(401).json({message:"no token!"})
+        }
+        jwt.verify(token, process.env.JWT_SECRET, (err,decoded) => {
+          if(err){
+            return res.status(401).json({message: "invalid token"})
+          }
+          req.token = decoded
+          next()
+        })
+   }catch(err){
+     res.status(401).json({ you: 'shall not pass!' });
+   }
+  
 };
