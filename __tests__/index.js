6335bee0ce0b951af0a0bf816e65708c41c059b0
Chris Forrest <redrider_cr@yahoo.com>
2020-09-11 21:53:32 -0400

    mvp met

--- __tests__/index.js
+++ __tests__/index.js
@@ -15,9 +15,13 @@ describe("user auth testing", () => {
     it("POST /api/auth/register", async() => {
         const res = await supertest(server)
                 .post("/api/auth/register")
-                .send({ usernmae:"kash", password: "whatup"})
+                .send({ username:"kash", password: "whatup"})
             expect(res.statusCode).toBe(201)
             expect(res.type).toBe("application/json")
             expect(res.body.username).toBe("kash") 
+    })
+
+    it("POST /api/auth/login", async () => {
+        const res = await (await supertest(server).post("/api/auth/login")).setEncoding({username:"kash", password:"whatup"})
     })
 })
